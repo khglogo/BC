0e63d0c261df43ed76d739397ae093eefdd38642
khglogo <67646827+khglogo@users.noreply.github.com>
2020-07-03 16:49:14 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "#";
+var prefix = "$";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
